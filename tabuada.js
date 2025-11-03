c3d92cbba6e6a46c4ce3160eb145511cddfd2ad1
AndreyMax66 <andreymaxmc@gmail.com>
2025-11-03 12:31:29 -0300

    estudando while também

--- tabuada.js
+++ tabuada.js
@@ -1,6 +1,17 @@
+// numero = prompt(`Digite o número que você deseja que a tabuada seja mostrada`)
+
+// for (let contador = 1; contador <=10; contador++){
+//     resultado = numero * contador
+//     console.log(`${numero} X ${contador} = ${resultado}`)
+
+// }
+
 numero = prompt(`Digite o número que você deseja que a tabuada seja mostrada`)
+let contador1 = 0
 
-for (let contador = 1; contador <=10; contador++){
-    console.log()
+while (contador1 <=10){
+    let resultado = numero * contador1
+    console.log(`${numero} X ${contador1} = ${resultado}`)
+    contador1++
+    
 }
-
