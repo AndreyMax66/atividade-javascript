d2130bfb6b066c91e63b10e29778c91febfd75c8
AndreyMax66 <andreymaxmc@gmail.com>
2025-11-03 12:01:15 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// WHILE
+
+
+
+// let contador = 1
+
+// while(contador <=10){
+
+//     console.log(`o numero contado é ${contador}`)
+//     contador++
+// }
+// let saldo = 1000
+
+// while(saldo > 0){
+//     saldo = saldo - 100
+//     console.log(saldo)
+// }
+
+
+let numeroSecreto = 65
+let tentativa = 0
+let chute;
+
+// while (chute != numeroSecreto && tentativa < 3) {
+    
+//     chute = Number(prompt(`tente adivinhar o numero secreto entre 1 e 100`))
+//     if (chute == numeroSecreto) {
+//         alert(`parabens, você acertou o número secreto`)
+//     }
+//     else {
+//         alert(`tente novamente`)
+//     }
+    
+//     tentativa++
+// }
+
+
+
+
+
+// ao testar um dos exercícios, perdi todo meu script.js devido aos travamentos do PC. Só consegui refazer esses 2 exercícios. 
---
+++ tabuada.js
@@ -0,0 +1,6 @@
+numero = prompt(`Digite o número que você deseja que a tabuada seja mostrada`)
+
+for (let contador = 1; contador <=10; contador++){
+    console.log()
+}
+
